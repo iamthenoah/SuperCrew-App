a848bb062569453ee3be6f752de25199e74408b2
Noah Greff <greffnoah@gmail.com>
2020-12-24 00:19:39 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<WindowHeader />
+	<router-view/>
+</template>
+
+<script lang="ts">
+
+	import { defineComponent } from 'vue';
+	import WindowHeader from '@/components/WindowHeader.vue';
+
+	export default defineComponent({
+		components: {
+			WindowHeader,
+		},
+	});
+
+</script>
+
+<style lang="scss">
+	@import './src/assets/styles/variables.scss';
+	@import './src/assets/styles/default.scss';
+	@import './src/assets/styles/main.scss';
+	@import './src/assets/styles/font.scss';
+</style>
---
+++ src/components/WindowHeader.vue
@@ -0,0 +1,98 @@
+<template>
+    <header>
+        <div class="app-name">SuperCrew</div>
+        <div class="titlebar-controls">
+            <div @click="minimize" class="titlebar-minimize">
+                <svg x="0px" y="0px" viewBox="0 0 10 1"><rect fill="#000000" width="10" height="1"></rect></svg>
+            </div>
+            <div @click="close" class="titlebar-close">
+                <svg x="0px" y="0px" viewBox="0 0 10 10"><polygon fill="#000000" points="10,1 9,0 5,4 1,0 0,1 4,5 0,9 1,10 5,6 9,10 10,9 6,5"></polygon></svg>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script lang="ts">
+
+    import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        methods: {
+            minimize: function() {
+                console.log('MIN');
+            },
+            close: function() {
+                console.log('QIT');
+            }
+        },
+    });
+
+</script>
+
+<style lang="scss" scoped>
+    
+	@import './src/assets/styles/variables.scss';
+
+    header {
+        position: absolute;
+        background: $darker;
+        width: 100%;
+        height: 22px;
+        user-select: none;
+        -webkit-app-region: drag;
+    }
+
+    .app-name {
+        margin: 3px 7.5px;
+        color: rgba(white, 0.5);
+        font-size: 13px;
+        text-transform: uppercase;
+        font-family: 'Roboto';
+    }
+
+    .titlebar-controls {
+        z-index: 1000;
+        position: absolute;
+        top: 0;
+        right: 0;
+        -webkit-app-region: no-drag;
+    }
+
+    .titlebar-minimize,
+    .titlebar-close {
+        width: 22px;
+        height: 22px;
+        float: left;
+        text-align: center;
+        line-height: 23px;
+        transition: background-color .1s;
+    }
+
+    .titlebar-minimize svg,
+    .titlebar-close svg {
+        width: 10px;
+        height: 10px;
+    }
+
+    .titlebar-close svg polygon {
+        transition: fill .1s;
+    }
+
+    .titlebar-close:hover svg polygon {
+        fill: white;
+    }
+
+    .titlebar-close:hover {
+        background: $red;
+    }
+
+    .titlebar-minimize:hover {
+        background: rgba(white, 0.1);
+    }
+
+    svg polygon,
+    svg rect {
+        fill: rgba(white, 0.5);
+    }
+
+</style>
