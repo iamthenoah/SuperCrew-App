2f1a4470021c4c9f790931c040326b89e8ebcc5c
Noah Greff <greffnoah@gmail.com>
2021-01-10 01:20:25 +0100

    changed animation to THE traditional loading bar animation

--- src/components/LoadingBar.vue
+++ src/components/LoadingBar.vue
@@ -9,17 +9,26 @@
 	#loading-bar {
 		position: absolute;
 		z-index: 1000;
-        width: 100%;
         height: 4px;
-		animation: load 1000ms ease-in-out alternate-reverse infinite;
+		animation: load 1000ms ease-out infinite;
         background: $theme;
     }
 
     @keyframes load {
 		0% {
-			left: -100%;
+			width: 0;
+			left: 0;
+		}
+		40% {
+			width: 75%;
+			left: 0;
+		}
+		80% {
+			width: 0;
+			left: 100%;
 		}
 		100% {
+			width: 0;
 			left: 100%;
 		}
     }
