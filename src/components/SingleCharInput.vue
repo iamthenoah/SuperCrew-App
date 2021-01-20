95d47953b6bbfa2b3961d06886504b15a8c0a9c9
Noah Greff <greffnoah@gmail.com>
2021-01-20 14:37:40 +0100

    addded component

---
+++ src/components/SingleCharInput.vue
@@ -0,0 +1,37 @@
+<template>
+	<input @click="listenForInput()" @keypress="changeInput()" class="" :value="inputChar" disabled>
+</template>
+
+<script lang="ts">
+
+	import { defineComponent } from 'vue';
+
+    export default defineComponent({
+        emits: [ 'onCharChanged' ],
+		data() {
+			return {
+				isListening: false,
+				char: null
+			}
+		},
+		props: {
+            inputChar: null
+		},
+		methods: {
+            chageInput: function() {
+				this.isListening = false;
+                this.$emit('onCharChanged', );
+            },
+            listenForInput: function() {
+                this.isListening = true;
+            }
+		},
+    });
+
+</script>
+
+<style lang="scss" scoped>
+
+	@import './src/assets/styles/variables.scss';
+
+</style>
