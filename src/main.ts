0070bbd4d6f7fbd9dccef5788304814bb3e39bac
greffnoah <greffnoah@gmail.com>
2021-02-04 12:02:59 +0100

    removed unnecessary commented line

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import router from './router';
 
 import VueClickOutside from './directives/click-outside';
 import VueTooltip from './directives/tooltip';
-// import tooltip from './directives/tooltip';
 
 const app = createApp(App);
 
